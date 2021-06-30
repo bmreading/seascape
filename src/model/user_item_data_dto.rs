b6374c9019cd88c89ea05c078b4358a216d6448e
Brian Reading <brian.reading@gmail.com>
2021-06-29 23:09:51 -0500

    Fix auto-gen doc comments

--- src/model/user_item_data_dto.rs
+++ src/model/user_item_data_dto.rs
@@ -1,43 +1,43 @@
 use serde::{Deserialize, Serialize};
 
-/// UserItemDataDto : Class UserItemDataDto.
+/// Data regarding a user item.
 
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct UserItemDataDto {
-    /// Gets or sets the rating.
+    /// The rating.
     #[serde(rename = "Rating", skip_serializing_if = "Option::is_none")]
     pub rating: Option<f64>,
-    /// Gets or sets the played percentage.
+    /// The played percentage.
     #[serde(rename = "PlayedPercentage", skip_serializing_if = "Option::is_none")]
     pub played_percentage: Option<f64>,
-    /// Gets or sets the unplayed item count.
+    /// The unplayed item count.
     #[serde(rename = "UnplayedItemCount", skip_serializing_if = "Option::is_none")]
     pub unplayed_item_count: Option<i32>,
-    /// Gets or sets the playback position ticks.
+    /// The playback position ticks.
     #[serde(
         rename = "PlaybackPositionTicks",
         skip_serializing_if = "Option::is_none"
     )]
     pub playback_position_ticks: Option<i64>,
-    /// Gets or sets the play count.
+    /// The play count.
     #[serde(rename = "PlayCount", skip_serializing_if = "Option::is_none")]
     pub play_count: Option<i32>,
-    /// Gets or sets a value indicating whether this instance is favorite.
+    /// A value indicating whether this instance is favorite.
     #[serde(rename = "IsFavorite", skip_serializing_if = "Option::is_none")]
     pub is_favorite: Option<bool>,
-    /// Gets or sets a value indicating whether this MediaBrowser.Model.Dto.UserItemDataDto is likes.
+    /// A value indicating whether this MediaBrowser.Model.Dto.UserItemDataDto is likes.
     #[serde(rename = "Likes", skip_serializing_if = "Option::is_none")]
     pub likes: Option<bool>,
-    /// Gets or sets the last played date.
+    /// The last played date.
     #[serde(rename = "LastPlayedDate", skip_serializing_if = "Option::is_none")]
     pub last_played_date: Option<String>,
-    /// Gets or sets a value indicating whether this MediaBrowser.Model.Dto.UserItemDataDto is played.
+    /// A value indicating whether this MediaBrowser.Model.Dto.UserItemDataDto is played.
     #[serde(rename = "Played", skip_serializing_if = "Option::is_none")]
     pub played: Option<bool>,
-    /// Gets or sets the key.
+    /// The key.
     #[serde(rename = "Key", skip_serializing_if = "Option::is_none")]
     pub key: Option<String>,
-    /// Gets or sets the item identifier.
+    /// The item identifier.
     #[serde(rename = "ItemId", skip_serializing_if = "Option::is_none")]
     pub item_id: Option<String>,
 }
